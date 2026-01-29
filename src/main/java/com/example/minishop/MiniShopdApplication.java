45a458f51da11c4aa32e8d1c7d16ca727796a446
Muhammad <mahkmadovm1999@gmail.com>
2026-01-29 16:51:38 +0900

    Initial commit

---
+++ src/main/java/com/example/minishop/MiniShopdApplication.java
@@ -0,0 +1,13 @@
+package com.example.minishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniShopdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniShopdApplication.class, args);
+	}
+
+}
